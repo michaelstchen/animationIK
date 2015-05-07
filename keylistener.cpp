0256e7e2144cf5f64475ecae1b83328402521480
Michael Chen <michaelstchen@gmail.com>
2015-05-07 00:49:52 -0700

    fixed I/O transformations

--- keylistener.cpp
+++ keylistener.cpp
@@ -3,6 +3,8 @@
 
 #include <GL/glut.h>
 
+#include <cmath>
+
 #include "keylistener.h"
 
 bool wireframemode = false;
@@ -18,16 +20,10 @@ void normalKeys(unsigned char key, int x, int y) {
     if (key == 27) {
         exit(0);
     } else if (key == 61) {
-        //viewMat = glm::scale(viewMat, vec3(1.05f, 1.05f, 1.05f));
-        mat4 rot = glm::scale(mat4(1.0f), vec3(0.95f, 0.95f, 0.95f));
-        vec4 camPos4 = rot * vec4(camPos, 1.0f);
-        camPos = vec3(camPos4[0], camPos4[1], camPos4[2]);
+        camPos = camPos * 0.95f;
         viewMat = glm::lookAt(camPos, origin, up);
     } else if (key == 45) {
-        //viewMat = glm::scale(viewMat, vec3(0.95f, 0.95f, 0.95f));
-        mat4 rot = glm::scale(mat4(1.0f), vec3(1.05f, 1.05f, 1.05f));
-        vec4 camPos4 = rot * vec4(camPos, 1.0f);
-        camPos = vec3(camPos4[0], camPos4[1], camPos4[2]);
+        camPos = camPos * 1.05f;
         viewMat = glm::lookAt(camPos, origin, up);
     } else if (key == 99) {
         wireframemode = !wireframemode;
@@ -39,28 +35,55 @@ void specialKeys(int key, int x, int y) {
 
     if (key == GLUT_KEY_RIGHT) {
         if (mod == GLUT_ACTIVE_SHIFT) {
-            viewMat = glm::translate(viewMat, right * 0.25f);
+            camPos = camPos + (-right * 0.25f);
+            origin = origin + (-right * 0.25f);
+            viewMat = glm::lookAt(camPos, origin, up);
         } else {
-            mat4 rot4 = glm::rotate(mat4(1.0f), degToRad(2.0f), up);
-            
+            camPos = camPos - origin;
+            mat4 rot = glm::rotate(mat4(1.0f), degToRad(2.0f), up);
+            right = vec3(rot * vec4(right, 1.0f));
+            camPos = vec3(rot * vec4(camPos, 1.0f));
+            camPos = camPos + origin;
+            viewMat = glm::lookAt(camPos, origin, up);
         }
     } else if (key == GLUT_KEY_LEFT) {
         if (mod == GLUT_ACTIVE_SHIFT) {
-            viewMat = glm::translate(viewMat, -right * 0.25f);
+            camPos = camPos + (right * 0.25f);
+            origin = origin + (right * 0.25f);
+            viewMat = glm::lookAt(camPos, origin, up);
         } else {
-
+            camPos = camPos - origin;
+            mat4 rot = glm::rotate(mat4(1.0f), degToRad(-2.0f), up);
+            right = vec3(rot * vec4(right, 1.0f));
+            camPos = vec3(rot * vec4(camPos, 1.0f));
+            camPos = camPos + origin;
+            viewMat = glm::lookAt(camPos, origin, up);
         }
     } else if (key == GLUT_KEY_UP) {
         if (mod == GLUT_ACTIVE_SHIFT) {
-            viewMat = glm::translate(viewMat, up * 0.25f);
+            camPos = camPos + (-up * 0.25f);
+            origin = origin + (-up * 0.25f);
+            viewMat = glm::lookAt(camPos, origin, up);
         } else {
-
+            camPos = camPos - origin;
+            mat4 rot = glm::rotate(mat4(1.0f), degToRad(-2.0f), right);
+            up = vec3(rot * vec4(up, 1.0f));
+            camPos = vec3(rot * vec4(camPos, 1.0f));
+            camPos = camPos + origin;
+            viewMat = glm::lookAt(camPos, origin, up);
         }
     } else if (key == GLUT_KEY_DOWN) {
         if (mod == GLUT_ACTIVE_SHIFT) {
-            viewMat = glm::translate(viewMat, -up * 0.25f);
+            camPos = camPos + (up * 0.25f);
+            origin = origin + (up * 0.25f);
+            viewMat = glm::lookAt(camPos, origin, up);
         } else {
-            
+            camPos = camPos - origin;
+            mat4 rot = glm::rotate(mat4(1.0f), degToRad(2.0f), right);
+            up = vec3(rot * vec4(up, 1.0f));
+            camPos = vec3(rot * vec4(camPos, 1.0f));
+            camPos = camPos + origin;
+            viewMat = glm::lookAt(camPos, origin, up);
         }
     }
 }
